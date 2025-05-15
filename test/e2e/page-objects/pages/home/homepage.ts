a6aaab7df2dacc38a5a140fd1ab4fb723a382d33
Howard Braham <howrad@gmail.com>
2025-05-15 17:23:14 -0400

    linting

--- test/e2e/page-objects/pages/home/homepage.ts
+++ test/e2e/page-objects/pages/home/homepage.ts
@@ -185,7 +185,7 @@ class HomePage {
     );
   }
 
-  async check_basicFunctionalityOffWarnigMessageIsDisplayed(): Promise<void> {
+  async check_basicFunctionalityOffWarningMessageIsDisplayed(): Promise<void> {
     console.log(
       'Check if basic functionality off warning message is displayed on homepage',
     );
